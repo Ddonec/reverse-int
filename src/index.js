07269bb6e0f1b995bff0f013f940c1ea77c72323
Anton <137609245+Ddonec@users.noreply.github.com>
2023-09-18 00:57:01 +0300

    feat: complede task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        console.log(n);
+        n = n * -1;
+        n = String(n);
+
+        const reverse = n.split("").reverse().join("");
+        console.log(reverse);
+        return reverse;
+    } else {
+        n = String(n);
+        const reverse = n.split("").reverse().join("");
+        console.log(reverse);
+        return reverse;
+    }
+};
